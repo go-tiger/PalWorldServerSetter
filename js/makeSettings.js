69d4b63bd660d2a9174e7607dac1e44e8a156022
go-tiger <inter_tiger_@naver.com>
2024-02-05 20:20:24 +0900

    feat: 설정값 클립보드에 저장

---
+++ js/makeSettings.js
@@ -0,0 +1,63 @@
+let AllSettingValue = {}
+
+function SaveSettings() {
+    for (const option in defaultSettings) {
+        const SettingValue = document.querySelector(`input[name="${option}"]`)
+
+        if (SettingValue.type == 'number') {
+            AllSettingValue[option] = SettingValue.value
+        }
+
+        if (SettingValue.type == 'radio') {
+            const SettingRadio = document.querySelectorAll(`input[name="${option}"]`)
+
+            for (let i = 0; i < SettingRadio.length; i++) {
+                if (SettingRadio[i].checked == true) {
+                    AllSettingValue[option] = SettingRadio[i].value
+                }
+            }
+        }
+
+        if (SettingValue.type == 'text') {
+            AllSettingValue[option] = SettingValue.value
+        }
+    }
+    return settingsContent(AllSettingValue)
+}
+
+function settingsContent(AllSettingValue) {
+    let PalWorldSettings = 
+`; This configuration file is a sample of the default server settings.
+; Changes to this file will NOT be reflected on the server.
+; To change the server settings, modify Pal/Saved/Config/WindowsServer/PalWorldSettings.ini.
+[/Script/Pal.PalGameWorldSettings]
+OptionSettings=(`
+
+    for (const option in AllSettingValue) {
+        if (AllSettingValue[option] === "") {
+            PalWorldSettings += `${option}="",`;
+        } else {
+            PalWorldSettings += `${option}=${AllSettingValue[option]},`;
+        }
+    }
+
+    PalWorldSettings = PalWorldSettings.slice(0, -1); //마지막 , 제거
+    PalWorldSettings += ")\n";
+
+    return PalWorldSettings
+}
+
+function copyClipboard(text) {
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            alert('설정이 복사되었습니다.')
+        })
+        .catch((err) => {
+            console.error("Error:", err);
+        });
+}
+
+function CopySettings() {
+    let PalWorldSettings = SaveSettings()
+    copyClipboard(PalWorldSettings)
+}
